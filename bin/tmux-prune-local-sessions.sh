d4349804e3ba02032e3441851812d894047b7ea4
Adam S. Crane <adam.crane@sas.com>
2014-03-16 01:40:02 -0400

    handle null cases for tmux pruning script

--- bin/tmux-prune-local-sessions.sh
+++ bin/tmux-prune-local-sessions.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+# script to kill detached local sessions (from closed tabs, etc)
 
 #TODO: replace with bash env-vars setup script when broken out from .bashrc
-if [ "$(uname -s)" = "Darwin" ]; then 
+if [[ $(uname -s) = "Darwin" ]]; then 
     if [[ -x /usr/libexec/path_helper ]]; then
-        eval `/usr/libexec/path_helper -s`
+        eval $(/usr/libexec/path_helper -s)
     fi
     export TMPDIR="$(getconf DARWIN_USER_TEMP_DIR)"
 fi
 
 if which tmux &> /dev/null; then
-    tmux start \; ls | grep local | grep -v attached | cut -d : -f 1 | xargs -L 1 tmux kill-session -t
+    for orphaned_local_session in $(tmux start \; ls | grep local | \
+        grep -v attached | cut -d : -f 1)
+    do
+        tmux kill-session -t $orphaned_local_session
+    done
 fi
